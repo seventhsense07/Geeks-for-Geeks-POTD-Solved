72839554152a3671a433e5eacf2b6dd0885c607c
Code Geeks Gang <118510312+seventhsense07@users.noreply.github.com>
2022-12-27 09:29:12 +0530

    Create POTD 27-12-2022.cpp

---
+++ December 2022/POTD 27-12-2022.cpp
@@ -0,0 +1,14 @@
+long long maxArea(long long A[], int len)
+{
+    // Your code goes here
+    int l = 0, r=len-1, h =0;
+    long long ans = 0;
+    while(l<r)
+    {
+        while(A[l] <= h )l++;
+        while(A[r] <= h) r--;
+        h = min(A[l],A[r]);
+        ans =  max(ans, (long long)(r-l)*h);
+    }
+    return ans;
+}
