29d5900f840e0e8b5a56a1370898e492db2aa13f
Code Geeks Gang <118510312+seventhsense07@users.noreply.github.com>
2023-01-20 09:04:17 +0530

    Create POTD 20-01-2023.cpp

---
+++ January 2023/POTD 20-01-2023.cpp
@@ -0,0 +1,43 @@
+class Solution
+{
+  public:
+  int maxWeightCell(int N, vector<int> Edge)
+  {
+      // code here
+      vector<int> A(N,0);
+
+      for(int i=0; i<N; i++){
+
+          if(Edge[i]!=-1){
+
+              A[Edge[i]] += i;
+
+          }
+
+          else 
+
+          continue;
+
+      }
+
+      
+
+      int maxe = INT_MIN;
+
+      int ans = 0;
+
+      for(int i=0; i<N; i++){
+
+          if(maxe<A[i]){
+
+              maxe = A[i];
+
+              ans = i;
+
+          }
+
+      }
+
+      return ans;
+  }
+};
